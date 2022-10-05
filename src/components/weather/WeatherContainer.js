ef60056c09373923d65bbb1eb43ce000493882f4
Matias Aguirre <aguirremati09@gmail.com>
2022-10-04 22:36:44 -0300

    Fixed Bugs

--- src/components/weather/WeatherContainer.js
+++ src/components/weather/WeatherContainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import getWeather from '../../redux/weather/weather'; // eslint-disable-line
+import getWeather from '../../redux/Weather/weather';
 import WeatherDetails from './Weatherdetails';
 import Spinner from '../spinners/Spinner';
 import './Weather.css';
