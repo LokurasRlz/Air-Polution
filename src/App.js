d628f73b2b10e847c01abe28f31469ced4f51006
Matias Aguirre <aguirremati09@gmail.com>
2022-10-05 11:48:58 -0300

    Fixed Deploy

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<AutoForm />} />
           <Route path="/details/:city" element={<WeatherContainer />} />
